3aa1e4b9309bb27a96f234f40c70f2e1e53f27de
haonhat0703 <haonhat2729@gmail.com>
2023-02-26 17:54:10 +0700

    dao nguoc chuoi

--- dng.asm
+++ dng.asm
@@ -35,5 +35,36 @@ main:
 	li $v0, 11
 	move $a0,$a1
 	syscall
+		
+	#Dao nguoc chuoi
+	#print 
+	li $v0, 4
+	la $a0, s2
+	syscall	
 	
+	#get length string
+	strlen:
+		li $v0, 0
+		la $a0, input
+		loop:
+			lb $t1, ($a0)
+			beq $t1, 10, exit#10 la dau xuong dong do nhap tu ban phim
+			#beqz $t1, exit #neu chuoi mac dinh
+			addi $a0, $a0, 1
+			addi $v0, $v0, 1
+			j loop
 	
+	exit:
+		#print nguoc
+		move $s0, $v0
+		la $t0, input
+		add $t0, $t0, $s0
+		lop:
+			li $v0, 11
+			lb $a0, ($t0)
+			syscall
+			subi $s0, $s0, 1
+			subi $t0, $t0, 1
+			beq $s0, -1, re
+			j lop
+	re:
